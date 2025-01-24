87be9584c79ede40328051a91175741d32eb9165
Elizabeth Okada <zabillet@gmail.com>
2025-01-24 15:12:50 -0800

    exit code 0

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    if input.trim() == "exit 0".to_string() {
+        std::process::exit(0);
+    }
+    else {
     println!("{}: command not found", input.trim())
     }
+}
 }

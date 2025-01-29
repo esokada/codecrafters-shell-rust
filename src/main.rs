0f2ad86ff4c90c1e903b91a98292cd9c2bdfbb17
Elizabeth Okada <zabillet@gmail.com>
2025-01-29 08:54:33 -0800

    added type

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,23 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
+    // define commands
+    // let commands = vec!["exit","echo","type"];
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
     match input.trim() {
         "exit 0" => std::process::exit(0),
-        input if input.trim().starts_with("echo") => println!("{}",input.trim().replace("echo","")),
-        input => println!("{}: command not found",input.trim())
+        input if input.trim().starts_with("echo") => 
+        println!("{}",input.trim().replace("echo ","")),
+        input if input.trim().starts_with("type") => 
+        match input.trim().replace("type ","").as_str()
+            {
+                "exit" | "echo" | "type" => println!("{} is a shell builtin",input.trim().replace("type ","")),
+                _ => println!("{}: not found",input.trim().replace("type ",""))
+            },
+            input => println!("{}: command not found",input.trim())
+            }
     }
     }
-}
